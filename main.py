b8537af0d050b398b7cb526855a0270598a79142
nguyenvanvutlv <nguyenvanvu.tlv@gmail.com>
2022-10-24 18:06:21 +0700

    update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ fx, base, const, saveBonus, aux = Input(source_data)
 order = [0] * len(base)
 for i, element in enumerate(base):
     order[[index+1 for index, value in enumerate(const) if value[0][element-1] == 1][0]-1] = element
-
+# Output(fx, const, saveBonus, base, aux + base)
 data = pd.read_csv("test.csv")
 
 print(data)
