4df44ada8eff480a591b7762fce7f5be5c681d11
nguyenvanvutlv <nguyenvanvu.tlv@gmail.com>
2022-10-24 18:04:21 +0700

    update

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import pandas as pd
 from getData import Input
 from outData import Output
 
 source_data = "data.txt"
 fx, base, const, saveBonus, aux = Input(source_data)
-Output(fx, const, saveBonus, base, aux + base)
+order = [0] * len(base)
+for i, element in enumerate(base):
+    order[[index+1 for index, value in enumerate(const) if value[0][element-1] == 1][0]-1] = element
+
+data = pd.read_csv("test.csv")
+
+print(data)
